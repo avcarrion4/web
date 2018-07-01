3c7435b65d954c9a8207618b96a1af0b8c0c1280
CRISTIAN JOSÉ MACAS PINEDA <cjmacas@utpl.edu.ec>
2018-07-01 17:02:36 -0500

    Base de datos

---
+++ Base de datos/web.sql
@@ -0,0 +1,409 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-07-2018 a las 00:01:46
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 5.6.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id_alumno` int(10) NOT NULL,
+  `nombre_alumno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido_alumno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cedula_alumno` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono_alumno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `correo_alumno` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `edad_alumno` int(10) NOT NULL,
+  `id_discapacidad` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `area`
+--
+
+CREATE TABLE `area` (
+  `id_area` int(10) NOT NULL,
+  `nombre_area` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `id_periodo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `discapacidad`
+--
+
+CREATE TABLE `discapacidad` (
+  `id_discapacidad` int(10) NOT NULL,
+  `nombre_discapacidad` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `porcentaje_discapacidad` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+CREATE TABLE `docente` (
+  `id_docente` int(10) NOT NULL,
+  `nombre_docente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido_docente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cedula_docente` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono_docente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `correo_docente` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `titulo_docente` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente_materia`
+--
+
+CREATE TABLE `docente_materia` (
+  `id_docmat` int(10) NOT NULL,
+  `id_docente` int(10) NOT NULL,
+  `id_materia` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE `materia` (
+  `id_materia` int(10) NOT NULL,
+  `nombre_materia` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Paralelo` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `id_titulacion` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia_alumno`
+--
+
+CREATE TABLE `materia_alumno` (
+  `id_matalu` int(10) NOT NULL,
+  `id_materia` int(10) NOT NULL,
+  `id_alumno` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo`
+--
+
+CREATE TABLE `periodo` (
+  `id_periodo` int(10) NOT NULL,
+  `anio` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `detalle` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pregunta`
+--
+
+CREATE TABLE `pregunta` (
+  `id_pregunta` int(10) NOT NULL,
+  `titulo_pregunta` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `id_prueba` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prueba`
+--
+
+CREATE TABLE `prueba` (
+  `id_prueba` int(10) NOT NULL,
+  `nombre_prueba` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `estado_prueba` tinyint(1) NOT NULL,
+  `id_docente` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuesta`
+--
+
+CREATE TABLE `respuesta` (
+  `id_respuesta` int(10) NOT NULL,
+  `opcion_respuesta` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `verificacion` tinyint(1) NOT NULL,
+  `id_pregunta` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `titulacion`
+--
+
+CREATE TABLE `titulacion` (
+  `id_titulacion` int(10) NOT NULL,
+  `nombre_titulacion` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `id_area` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD KEY `fk_alumno_discapacidad` (`id_discapacidad`);
+
+--
+-- Indices de la tabla `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`id_area`),
+  ADD KEY `fk_area_periodo` (`id_periodo`);
+
+--
+-- Indices de la tabla `discapacidad`
+--
+ALTER TABLE `discapacidad`
+  ADD PRIMARY KEY (`id_discapacidad`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`id_docente`);
+
+--
+-- Indices de la tabla `docente_materia`
+--
+ALTER TABLE `docente_materia`
+  ADD PRIMARY KEY (`id_docmat`),
+  ADD KEY `fk_docente_materia` (`id_materia`),
+  ADD KEY `fk_materia_doccente` (`id_docente`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`id_materia`),
+  ADD KEY `fk_materia_titulacion` (`id_titulacion`);
+
+--
+-- Indices de la tabla `materia_alumno`
+--
+ALTER TABLE `materia_alumno`
+  ADD PRIMARY KEY (`id_matalu`),
+  ADD KEY `fk_materia_alumno` (`id_materia`),
+  ADD KEY `fk_alumno_materia` (`id_alumno`);
+
+--
+-- Indices de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  ADD PRIMARY KEY (`id_periodo`);
+
+--
+-- Indices de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`id_pregunta`),
+  ADD KEY `fk_pregunta_prueba` (`id_prueba`);
+
+--
+-- Indices de la tabla `prueba`
+--
+ALTER TABLE `prueba`
+  ADD PRIMARY KEY (`id_prueba`),
+  ADD KEY `fk_prueba_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD PRIMARY KEY (`id_respuesta`),
+  ADD KEY `fk_respuesta_pregunta` (`id_pregunta`);
+
+--
+-- Indices de la tabla `titulacion`
+--
+ALTER TABLE `titulacion`
+  ADD PRIMARY KEY (`id_titulacion`),
+  ADD KEY `fk_titulacion_area` (`id_area`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id_alumno` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `area`
+--
+ALTER TABLE `area`
+  MODIFY `id_area` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `discapacidad`
+--
+ALTER TABLE `discapacidad`
+  MODIFY `id_discapacidad` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `id_docente` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `docente_materia`
+--
+ALTER TABLE `docente_materia`
+  MODIFY `id_docmat` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `id_materia` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `materia_alumno`
+--
+ALTER TABLE `materia_alumno`
+  MODIFY `id_matalu` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  MODIFY `id_periodo` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  MODIFY `id_pregunta` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prueba`
+--
+ALTER TABLE `prueba`
+  MODIFY `id_prueba` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  MODIFY `id_respuesta` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `titulacion`
+--
+ALTER TABLE `titulacion`
+  MODIFY `id_titulacion` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD CONSTRAINT `fk_alumno_discapacidad` FOREIGN KEY (`id_discapacidad`) REFERENCES `discapacidad` (`id_discapacidad`);
+
+--
+-- Filtros para la tabla `area`
+--
+ALTER TABLE `area`
+  ADD CONSTRAINT `fk_area_periodo` FOREIGN KEY (`id_periodo`) REFERENCES `periodo` (`id_periodo`);
+
+--
+-- Filtros para la tabla `docente_materia`
+--
+ALTER TABLE `docente_materia`
+  ADD CONSTRAINT `fk_docente_materia` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`),
+  ADD CONSTRAINT `fk_materia_doccente` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id_docente`);
+
+--
+-- Filtros para la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD CONSTRAINT `fk_materia_titulacion` FOREIGN KEY (`id_titulacion`) REFERENCES `titulacion` (`id_titulacion`);
+
+--
+-- Filtros para la tabla `materia_alumno`
+--
+ALTER TABLE `materia_alumno`
+  ADD CONSTRAINT `fk_alumno_materia` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  ADD CONSTRAINT `fk_materia_alumno` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`);
+
+--
+-- Filtros para la tabla `pregunta`
+--
+ALTER TABLE `pregunta`
+  ADD CONSTRAINT `fk_pregunta_prueba` FOREIGN KEY (`id_prueba`) REFERENCES `prueba` (`id_prueba`);
+
+--
+-- Filtros para la tabla `prueba`
+--
+ALTER TABLE `prueba`
+  ADD CONSTRAINT `fk_prueba_docente` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id_docente`);
+
+--
+-- Filtros para la tabla `respuesta`
+--
+ALTER TABLE `respuesta`
+  ADD CONSTRAINT `fk_respuesta_pregunta` FOREIGN KEY (`id_pregunta`) REFERENCES `pregunta` (`id_pregunta`);
+
+--
+-- Filtros para la tabla `titulacion`
+--
+ALTER TABLE `titulacion`
+  ADD CONSTRAINT `fk_titulacion_area` FOREIGN KEY (`id_area`) REFERENCES `area` (`id_area`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
