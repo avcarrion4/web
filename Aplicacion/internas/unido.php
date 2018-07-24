e61b1d5445c880750dabf2cd6b1548511b345c93
avcarrion4 <34322210+avcarrion4@users.noreply.github.com>
2018-07-24 03:42:33 -0500

    actual

--- Aplicacion/internas/unido.php
+++ Aplicacion/internas/unido.php
@@ -14,9 +14,19 @@
     $aux=$aux."$pregunta[titulo_pregunta]";
     $query="Select * from respuesta where id_pregunta="."$pregunta[id_pregunta]";
     $respuestas=mysql_query($query) or die('Error de sql');
+    $cont2=1;
     while ($respuesta=mysql_fetch_array($respuestas, MYSQL_ASSOC)) {
-      $aux=$aux."\n"."$respuesta[opcion_respuesta]";
+      if ($cont2==1) {
+        $aux=$aux."\nA) "."$respuesta[opcion_respuesta]";        
+      }
+      if ($cont2==2) {
+        $aux=$aux."\nB) "."$respuesta[opcion_respuesta]";
+      }
+      if ($cont2==3) {
+        $aux=$aux."\nC) "."$respuesta[opcion_respuesta]";
+      }
       
+      $cont2++;
 
     }
     $aux2=array($aux, 0, 0,0);
@@ -38,7 +48,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Texto a voz</title>
-	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<link rel="stylesheet" type="text/css" href="../css/css.css">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
 </head>
@@ -66,55 +76,172 @@
       <label for="pitch">Pitch</label>
       <input type="range" min="0" max="2" step="0.1" name="pitch" id="pitch" value="1" disabled>
     </div>
-    <button id="reproducir">Reproducir</button>  
+    
   </section>
 	<SECTION>
-  <?php 
-    $arrlength=count($array_preguntas);
-    for($x = 1; $x <= $arrlength; $x++) {
-      $aux3=$array_preguntas[$x];
-      echo $aux3[0].";".$aux3[1].";".$aux3[2].";".$aux3[3];
-      echo "<br>";
-    }
-  ?>
-
-    <label>PREGUNTA :</label>
-    <div>
-      <textarea type="text" name="speech-msg" id="texto" value="" rows="10" cols="80" x-webkit-speech><?php $aux3=$array_preguntas[1]; echo "$aux3[0]"; ?></textarea> 
-    </div>
+    <?php 
+      $arrlength=count($array_preguntas);
+      for($x = 1; $x <= $arrlength; $x++) {
+        $aux3=$array_preguntas[$x];
+        ?>
+        <label>PREGUNTA <?php echo "$x"; ?> :</label>
+        <textarea type="text" name="speech-msg" id="texto<?php echo $x?>" value="" rows="10" cols="80" x-webkit-speech><?php echo "$aux3[0]"; ?></textarea>
+        
+        <img onclick="accion(<?php echo $x?>);" src="../img/play-button.png">
+        <div id="resp">
+          <textarea rows="3" cols="80" id="interimResult<?php echo $x?>">
+            
+          </textarea>
+          <textarea rows="3" cols="80" id="finalResult<?php echo $x?>">
+            
+          </textarea>
+        </div>
+        <input type="button" onclick="accion2(<?php echo $x?>);" id="btn<?php echo $x?>" value="start"/>
+        <?php 
+        
+        echo "<br>";
+      }
+    ?>
+    <input type="button" onclick="validar(<?php echo $cont?>);" id="btn<?php echo $x?>" value="Guardar"/>
   </SECTION>
 
-	
-	<h1 class="center" id="headline">Respuesta</h1>
-    <form>
-      <input type="button" id="btn" value="start"/>
-    </form>
-    <div id="resp">
-    	<textarea rows="3" cols="80" id="interimResult">
-    		
-    	</textarea>
-    	<textarea rows="3" cols="80" id="finalResult">
-    		
-    	</textarea>
-    	
-
-		<script type="text/javascript" src="js/speech-recognition.js"></script>	
-    </div>
-
-  </SECTION>
+	 
  
 
 
 </div>
 
   <script type="text/javascript">
+    
+
+    function accion2(x){
+      
+      var finalTranscript = "";
+      var isRecognizing = false;
+      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+      var recognition = new SpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.lang = 'es_ES';
+      var aux1="interimResult"
+      aux1=aux1.concat(x);
+      var aux2="btn"
+      aux2=aux2.concat(x);
+      var aux3="finalResult"
+      aux3=aux3.concat(x);
+
+      
+      
+      var interimResult = document.getElementById(aux1);
+      var btn = document.getElementById(aux2);
+      var finalResult = document.getElementById(aux3);
+
+      if (btn.value=="stop") {
+        var isRecognizing = true;
+      }
+      
+      if (isRecognizing) {
+        recognition.stop();
+        btn.value="start to listen";
+      } else {  
+        finalTranscript = "";
+        finalResult.innerHTML = "";
+        interimResult.innerHTML = "";
+        btn.value="stop";
+        recognition.start();
+      }
+
+      recognition.onresult = function(event) {
+        var interimTranscript = "";
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+          var _transcript = event.results[i][0].transcript;
+          if (event.results[i].isFinal) {
+            finalTranscript += StringUtils.capitalize(_transcript) + ".";
+          } else {
+            interimTranscript += _transcript;
+          }
+        }
+        switch (x) {
+          case '0':
+          break;
+          case '1':
+            finalResult1.innerHTML = finalTranscript;
+            interimResult1.innerHTML = interimTranscript;
+          
+          break;
+          case '2':
+            finalResult2.innerHTML = finalTranscript;
+            interimResult2.innerHTML = interimTranscript;
+          break;
+          case '3':
+            finalResult3.innerHTML = finalTranscript;
+            interimResult3.innerHTML = interimTranscript;
+          break;
+          case '4':
+            finalResult4.innerHTML = finalTranscript;
+            interimResult4.innerHTML = interimTranscript;
+          break;
+          case '5':
+            finalResult5.innerHTML = finalTranscript;
+            interimResult5.innerHTML = interimTranscript;
+          break;
+          case '6':
+            finalResult6.innerHTML = finalTranscript;
+            interimResult6.innerHTML = interimTranscript;
+          break;
+          case '7':
+            finalResult7.innerHTML = finalTranscript;
+            interimResult7.innerHTML = interimTranscript;
+          break;
+          case '8':
+            finalResult8.innerHTML = finalTranscript;
+            interimResult8.innerHTML = interimTranscript;
+          break;
+          case '9':
+            finalResult9.innerHTML = finalTranscript;
+            interimResult9.innerHTML = interimTranscript;
+          break;
+          case '10':
+            finalResult10.innerHTML = finalTranscript;
+            interimResult10.innerHTML = interimTranscript;
+          break;
+
+        }
+        finalResult.innerHTML = finalTranscript;
+        interimResult.innerHTML = interimTranscript;
+      };
+      recognition.onstart = function() {
+          isRecognizing = true;
+      };
+      recognition.onend = function() {
+        isRecognizing = false;
+        if (!finalTranscript) {
+          return;
+        }
+      };
+      recognition.onerror = function(event) {
+        console.log(event.error);
+        //'no-speech','audio-capture','not-allowed'
+      };
+
+
+      var StringUtils = {
+        FIRST_CHAR : /\S/,
+        capitalize : function(s) {
+          return s.replace(this.FIRST_CHAR, function(m) { return m.toUpperCase(); });
+        }
+      }
+    }
+  </script>
+
+  <script type="text/javascript">
 
 
     // Get the 'speak' button
-    var button = document.getElementById('reproducir');
+    //var button = document.getElementById('reproducir');
 
     // Get the text input element.
-    var speechMsgInput = document.getElementById('texto');
+    //var speechMsgInput = document.getElementById('texto1');
 
     // Get the voice select element.
     var voiceSelect = document.getElementById('voice');
@@ -141,6 +268,7 @@
     // Create a new utterance for the specified text and add it to
     // the queue.
     function speak(text) {
+
       // Create a new instance of SpeechSynthesisUtterance.
       var msg = new SpeechSynthesisUtterance();
       
@@ -164,15 +292,34 @@
 
 
     // Set up an event listener for when the 'speak' button is clicked.
-    function accion(){
+    function accion(x){
+      var aux1="texto"
+      aux1=aux1.concat(x);
+      alert(aux1);
+      var speechMsgInput = document.getElementById(aux1);
+
       if (speechMsgInput.value.length > 0) {
         speak(speechMsgInput.value);
       }
     }
-    
-
-    
   </script>
+
+  <script type="text/javascript">
+    function validar(cont){
+      var aux1="finalResult";
+      for (var i = 1; i < cont; i++) {
+        
+        aux1=aux1.concat(cont);
+        alert(aux1);  
+      }
+      
+
+      //var texta = document.getElementById();
+
+
+    }
+  </script>
+  <script type="text/javascript" src="../js/speech-recognition.js"></script>  
   
   
 </body>
